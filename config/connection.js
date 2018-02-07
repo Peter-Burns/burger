f05bf5e3a0c6e3c5baaa30bddff3d8246c3b61df
Pete Burns <peterburnshicks@gmail.com>
2018-02-07 01:52:29 -0500

    changed to jawsdb login

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: '	bdsx4cl2lt8x9lht',
+  password: '	yqtpplqzkury2pac',
   database: 'burgers_db'
 });
 
