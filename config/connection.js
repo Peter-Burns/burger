164386c38dbdcde9fccb57b2854454975f6bdecd
Pete Burns <peterburnshicks@gmail.com>
2018-02-07 02:07:22 -0500

    added jawsDB/local host switching

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: '	bdsx4cl2lt8x9lht',
-  password: '	yqtpplqzkury2pac',
-  database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
 
 connection.connect(function (err) {
   if (err) {
